223973bb9212e862d24b3be937fe5e8da585b720
Sabbir <prince3339@ymail.com>
2022-03-22 16:01:01 +0600

    Added a generic movie component for both movie list and watch list

---
+++ src/components/Movies.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Movies = ({movies, handleClick}) => {
+  return (
+    <div>
+      {movies?.map((movie, index) => (
+        <div className="movies" key={movie.title}>
+          <div>
+            <img src={movie.image} height="100px" alt={movie.title}/>
+          </div>
+          <span>
+            <button className="movie-watched" onClick={()=> {
+              handleClick(movie, index);
+            }}>
+              {movie.title}
+            </button>
+          </span>
+          <br />
+          <span>
+            {movie.comment}
+          </span>
+          <br />
+          <br />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Movies;
